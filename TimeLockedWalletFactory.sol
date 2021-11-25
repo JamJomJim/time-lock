6510f9f3cbfd29398929e9ec903934f23c681b66
jin <87555412+Jinchans@users.noreply.github.com>
2021-11-25 11:54:43 +0700

    Update TimeLockedWalletFactory.sol

--- TimeLockedWalletFactory.sol
+++ TimeLockedWalletFactory.sol
@@ -17,7 +17,7 @@ contract TimeLockedWalletFactory {
     function createTimeLockedWallet(address _owner, uint256 _unlockDate)
         payable
         public
-        returns(address wallet)
+        //returns(address wallet)
     {
         // Create new wallet.
         //wallet = new TimeLockedWallet(msg.sender, _owner, _unlockDate);
@@ -27,15 +27,15 @@ contract TimeLockedWalletFactory {
         wallets[msg.sender].push(payable(wallet));
 
         // If owner is the same as sender then add wallet to sender's wallets too.
-        if(msg.sender != _owner){
-            wallets[_owner].push(wallet);
+        if(payable(msg.sender) != _owner){
+            wallets[_owner].push(payable(wallet));
         }
 
         // Send ether from this transaction to the created contract.
         payable(wallet).transfer(msg.value);
 
         // Emit event.
-        emit Created(wallet, msg.sender, _owner, block.timestamp, _unlockDate, msg.value);
+        emit Created(address(wallet), msg.sender, _owner, block.timestamp, _unlockDate, msg.value);
     }
 
     // Prevents accidental sending of ether to the factory
